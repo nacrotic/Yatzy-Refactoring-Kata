cdc771cddc6b9737638fecfd6463d9643f3c497a
Leo <lperrin@norsys.fr>
2023-05-24 16:13:17 +0200

    chore(impl): Generalize roll object

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -9,103 +9,80 @@ import net.leoperrin.nickel.yatze.evaluator.TwoPairEvaluator;
 
 public class Yatzy {
 
-	public static int chance(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	private Yatzy() {
+	}
+	
+	public static int chance(Roll roll) {
 		Evaluator evaluator = new ChanceEvaluator();
 		return evaluator.getScore(roll);
 	}
 
-	public static int yatzy(int... dice) {
-		Roll roll = new Roll(dice[0], dice[1], dice[2], dice[3], dice[4]);
+	public static int yatzy(Roll roll) {
 		Evaluator evaluator = new OfKindEvaluator(5);
 		return evaluator.getScore(roll);
 	}
 
-	public static int ones(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int ones(Roll roll) {
 		Evaluator evaluator = new MaxOfEvaluator(1);
 		return evaluator.getScore(roll);
 	}
 
-	public static int twos(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int twos(Roll roll) {
 		Evaluator evaluator = new MaxOfEvaluator(2);
 		return evaluator.getScore(roll);
 	}
 
-	public static int threes(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int threes(Roll roll) {
 		Evaluator evaluator = new MaxOfEvaluator(3);
 		return evaluator.getScore(roll);
 	}
 
-	protected int[] dice;
-
-	public Yatzy(int d1, int d2, int d3, int d4, int _5) {
-		dice = new int[5];
-		dice[0] = d1;
-		dice[1] = d2;
-		dice[2] = d3;
-		dice[3] = d4;
-		dice[4] = _5;
-	}
-
-	public int fours() {
-		Roll roll = new Roll(dice[0], dice[1], dice[2], dice[3], dice[4]);
+	public static int fours(Roll roll) {
 		Evaluator evaluator = new MaxOfEvaluator(4);
 		return evaluator.getScore(roll);
 	}
 
-	public int fives() {
-		Roll roll = new Roll(dice[0], dice[1], dice[2], dice[3], dice[4]);
+	public static int fives(Roll roll) {
 		Evaluator evaluator = new MaxOfEvaluator(5);
 		return evaluator.getScore(roll);
 	}
 
-	public int sixes() {
-		Roll roll = new Roll(dice[0], dice[1], dice[2], dice[3], dice[4]);
+	public static int sixes(Roll roll) {
 		Evaluator evaluator = new MaxOfEvaluator(6);
 		return evaluator.getScore(roll);
 	}
 
-	public static int score_pair(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int score_pair(Roll roll) {
 		Evaluator evaluator = new OfKindEvaluator(2);
 		return evaluator.getScore(roll);
 	}
 
-	public static int two_pair(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int two_pair(Roll roll) {
 		Evaluator evaluator = new TwoPairEvaluator();
 		return evaluator.getScore(roll);
 	}
 
-	public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
-		Roll roll = new Roll(_1, _2, d3, d4, d5);
+	public static int four_of_a_kind(Roll roll) {
 		Evaluator evaluator = new OfKindEvaluator(4);
 		return evaluator.getScore(roll);
 	}
 
-	public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int three_of_a_kind(Roll roll) {
 		Evaluator evaluator = new OfKindEvaluator(3);
 		return evaluator.getScore(roll);
 	}
 
-	public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int smallStraight(Roll roll) {
 		Evaluator evaluator = new StraightEvaluator(false);
 		return evaluator.getScore(roll);
 	}
 
-	public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int largeStraight(Roll roll) {
 		Evaluator evaluator = new StraightEvaluator(true);
 		return evaluator.getScore(roll);
 	}
 
-	public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
-		Roll roll = new Roll(d1, d2, d3, d4, d5);
+	public static int fullHouse(Roll roll) {
 		Evaluator evaluator = new FullEvaluator();
 		return evaluator.getScore(roll);
 	}
--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -1,3 +1,4 @@
+import net.leoperrin.nickel.yatze.Roll;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,123 +7,123 @@ public class YatzyTest {
 
 	@Test
 	public void test_chance() {
-		assertEquals(15, Yatzy.chance(2, 3, 4, 5, 1));
-		assertEquals(16, Yatzy.chance(3, 3, 4, 5, 1));
-		assertEquals(5, Yatzy.chance(1, 1, 1, 1, 1));
+		assertEquals(15, Yatzy.chance(new Roll(2, 3, 4, 5, 1)));
+		assertEquals(16, Yatzy.chance(new Roll(3, 3, 4, 5, 1)));
+		assertEquals(5, Yatzy.chance(new Roll(1, 1, 1, 1, 1)));
 	}
 
 	@Test
 	public void test_yatzy() {
-		assertEquals(50, Yatzy.yatzy(4, 4, 4, 4, 4));
-		assertEquals(50, Yatzy.yatzy(6, 6, 6, 6, 6));
-		assertEquals(0, Yatzy.yatzy(6, 6, 6, 6, 3));
-		assertEquals(0, Yatzy.yatzy(1, 2, 3, 4, 5));
+		assertEquals(50, Yatzy.yatzy(new Roll(4, 4, 4, 4, 4)));
+		assertEquals(50, Yatzy.yatzy(new Roll(6, 6, 6, 6, 6)));
+		assertEquals(0, Yatzy.yatzy(new Roll(6, 6, 6, 6, 3)));
+		assertEquals(0, Yatzy.yatzy(new Roll(1, 2, 3, 4, 5)));
 	}
 
 	@Test
 	public void test_1s() {
-		assertEquals(1, Yatzy.ones(1, 2, 3, 4, 5));
-		assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
-		assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
-		assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
-		assertEquals(5, Yatzy.ones(1, 1, 1, 1, 1));
+		assertEquals(1, Yatzy.ones(new Roll(1, 2, 3, 4, 5)));
+		assertEquals(2, Yatzy.ones(new Roll(1, 2, 1, 4, 5)));
+		assertEquals(0, Yatzy.ones(new Roll(6, 2, 2, 4, 5)));
+		assertEquals(4, Yatzy.ones(new Roll(1, 2, 1, 1, 1)));
+		assertEquals(5, Yatzy.ones(new Roll(1, 1, 1, 1, 1)));
 	}
 
 	@Test
 	public void test_2s() {
-		assertEquals(4, Yatzy.twos(1, 2, 3, 2, 6));
-		assertEquals(10, Yatzy.twos(2, 2, 2, 2, 2));
-		assertEquals(0, Yatzy.twos(1, 3, 4, 5, 6));
+		assertEquals(4, Yatzy.twos(new Roll(1, 2, 3, 2, 6)));
+		assertEquals(10, Yatzy.twos(new Roll(2, 2, 2, 2, 2)));
+		assertEquals(0, Yatzy.twos(new Roll(1, 3, 4, 5, 6)));
 	}
 
 	@Test
 	public void test_3s() {
-		assertEquals(6, Yatzy.threes(1, 2, 3, 2, 3));
-		assertEquals(12, Yatzy.threes(2, 3, 3, 3, 3));
-		assertEquals(15, Yatzy.threes(3, 3, 3, 3, 3));
-		assertEquals(0, Yatzy.threes(1, 2, 4, 4, 6));
+		assertEquals(6, Yatzy.threes(new Roll(1, 2, 3, 2, 3)));
+		assertEquals(12, Yatzy.threes(new Roll(2, 3, 3, 3, 3)));
+		assertEquals(15, Yatzy.threes(new Roll(3, 3, 3, 3, 3)));
+		assertEquals(0, Yatzy.threes(new Roll(1, 2, 4, 4, 6)));
 	}
 
 	@Test
 	public void test_4s() {
-		assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
-		assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
-		assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
-		assertEquals(20, new Yatzy(4, 4, 4, 4, 4).fours());
-		assertEquals(0, new Yatzy(1, 2, 3, 5, 6).fours());
+		assertEquals(12, Yatzy.fours(new Roll(4, 4, 4, 5, 5)));
+		assertEquals(8, Yatzy.fours(new Roll(4, 4, 5, 5, 5)));
+		assertEquals(4, Yatzy.fours(new Roll(4, 5, 5, 5, 5)));
+		assertEquals(20, Yatzy.fours(new Roll(4, 4, 4, 4, 4)));
+		assertEquals(0, Yatzy.fours(new Roll(1, 2, 3, 5, 6)));
 	}
 
 	@Test
 	public void test_5s() {
-		assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
-		assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
-		assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
-		assertEquals(0, new Yatzy(1, 2, 3, 4, 6).fives());
-		assertEquals(25, new Yatzy(5, 5, 5, 5, 5).fives());
+		assertEquals(10, Yatzy.fives(new Roll(4, 4, 4, 5, 5)));
+		assertEquals(15, Yatzy.fives(new Roll(4, 4, 5, 5, 5)));
+		assertEquals(20, Yatzy.fives(new Roll(4, 5, 5, 5, 5)));
+		assertEquals(0, Yatzy.fives(new Roll(1, 2, 3, 4, 6)));
+		assertEquals(25, Yatzy.fives(new Roll(5, 5, 5, 5, 5)));
 	}
 
 	@Test
 	public void test_6s() {
-		assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
-		assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
-		assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
-		assertEquals(30, new Yatzy(6, 6, 6, 6, 6).sixes());
+		assertEquals(0, Yatzy.sixes(new Roll(4, 4, 4, 5, 5)));
+		assertEquals(6, Yatzy.sixes(new Roll(4, 4, 6, 5, 5)));
+		assertEquals(18, Yatzy.sixes(new Roll(6, 5, 6, 6, 5)));
+		assertEquals(30, Yatzy.sixes(new Roll(6, 6, 6, 6, 6)));
 	}
 
 	@Test
 	public void test_pair() {
-		assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6));
-		assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5));
-		assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5));
-		assertEquals(0, Yatzy.score_pair(1, 3, 2, 6, 5));
-		assertEquals(2, Yatzy.score_pair(1, 1, 1, 1, 1));
+		assertEquals(6, Yatzy.score_pair(new Roll(3, 4, 3, 5, 6)));
+		assertEquals(10, Yatzy.score_pair(new Roll(5, 3, 3, 3, 5)));
+		assertEquals(12, Yatzy.score_pair(new Roll(5, 3, 6, 6, 5)));
+		assertEquals(0, Yatzy.score_pair(new Roll(1, 3, 2, 6, 5)));
+		assertEquals(2, Yatzy.score_pair(new Roll(1, 1, 1, 1, 1)));
 	}
 
 	@Test
 	public void test_twoPair() {
-		assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5));
-		assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5));
-		assertEquals(0, Yatzy.two_pair(1, 3, 5, 2, 4));
-		assertEquals(18, Yatzy.two_pair(5, 3, 5, 4, 4));
-		assertEquals(0, Yatzy.two_pair(1, 1, 1, 1, 4));
+		assertEquals(16, Yatzy.two_pair(new Roll(3, 3, 5, 4, 5)));
+		assertEquals(16, Yatzy.two_pair(new Roll(3, 3, 5, 5, 5)));
+		assertEquals(0, Yatzy.two_pair(new Roll(1, 3, 5, 2, 4)));
+		assertEquals(18, Yatzy.two_pair(new Roll(5, 3, 5, 4, 4)));
+		assertEquals(0, Yatzy.two_pair(new Roll(1, 1, 1, 1, 4)));
 	}
 
 	@Test
 	public void test_threeOfAKind() {
-		assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
-		assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
-		assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
-		assertEquals(0, Yatzy.three_of_a_kind(1, 2, 3, 4, 5));
-		assertEquals(18, Yatzy.three_of_a_kind(6, 6, 6, 6, 6));
+		assertEquals(9, Yatzy.three_of_a_kind(new Roll(3, 3, 3, 4, 5)));
+		assertEquals(15, Yatzy.three_of_a_kind(new Roll(5, 3, 5, 4, 5)));
+		assertEquals(9, Yatzy.three_of_a_kind(new Roll(3, 3, 3, 3, 5)));
+		assertEquals(0, Yatzy.three_of_a_kind(new Roll(1, 2, 3, 4, 5)));
+		assertEquals(18, Yatzy.three_of_a_kind(new Roll(6, 6, 6, 6, 6)));
 	}
 
 	@Test
 	public void test_fourOfAKind() {
-		assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
-		assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
-		assertEquals(24, Yatzy.four_of_a_kind(6, 6, 6, 6, 6));
-		assertEquals(0, Yatzy.four_of_a_kind(6, 1, 2, 6, 6));
+		assertEquals(12, Yatzy.four_of_a_kind(new Roll(3, 3, 3, 3, 5)));
+		assertEquals(20, Yatzy.four_of_a_kind(new Roll(5, 5, 5, 4, 5)));
+		assertEquals(24, Yatzy.four_of_a_kind(new Roll(6, 6, 6, 6, 6)));
+		assertEquals(0, Yatzy.four_of_a_kind(new Roll(6, 1, 2, 6, 6)));
 	}
 
 	@Test
 	public void test_smallStraight() {
-		assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
-		assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
-		assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
-		assertEquals(0, Yatzy.smallStraight(2, 3, 4, 5, 6));
+		assertEquals(15, Yatzy.smallStraight(new Roll(1, 2, 3, 4, 5)));
+		assertEquals(15, Yatzy.smallStraight(new Roll(2, 3, 4, 5, 1)));
+		assertEquals(0, Yatzy.smallStraight(new Roll(1, 2, 2, 4, 5)));
+		assertEquals(0, Yatzy.smallStraight(new Roll(2, 3, 4, 5, 6)));
 	}
 
 	@Test
 	public void test_largeStraight() {
-		assertEquals(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
-		assertEquals(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
-		assertEquals(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
+		assertEquals(20, Yatzy.largeStraight(new Roll(6, 2, 3, 4, 5)));
+		assertEquals(20, Yatzy.largeStraight(new Roll(2, 3, 4, 5, 6)));
+		assertEquals(0, Yatzy.largeStraight(new Roll(1, 2, 2, 4, 5)));
 	}
 
 	@Test
 	public void test_fullHouse() {
-		assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
-		assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
-		assertEquals(0, Yatzy.fullHouse(2, 2, 2, 2, 2));
+		assertEquals(18, Yatzy.fullHouse(new Roll(6, 2, 2, 2, 6)));
+		assertEquals(0, Yatzy.fullHouse(new Roll(2, 3, 4, 5, 6)));
+		assertEquals(0, Yatzy.fullHouse(new Roll(2, 2, 2, 2, 2)));
 	}
 }
